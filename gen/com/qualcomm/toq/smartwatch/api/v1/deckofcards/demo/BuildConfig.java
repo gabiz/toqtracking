c66a93f0621b77133f3c8a99c06004772a58fe81
Gabi Zuniga <gabiz@alignd.io>
2014-04-27 13:43:38 -0700

    Initial Checking

---
+++ gen/com/qualcomm/toq/smartwatch/api/v1/deckofcards/demo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.qualcomm.toq.smartwatch.api.v1.deckofcards.demo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
